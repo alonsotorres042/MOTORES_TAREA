a57df80a6330b9a35abaa8434b449db6cb4f16b1
ALONSO TORRES <alonso.torres@tecsup.edu.pe>
2024-03-12 10:07:47 -0500

    TestGit

--- Assets/Scripts/SceneGoer.cs
+++ Assets/Scripts/SceneGoer.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class SceneController : MonoBehaviour
+
+public class SceneGoer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,8 +16,8 @@ public class SceneController : MonoBehaviour
     {
         
     }
-    public void GoToMethod(string GoTo)
+    public void LoadScenary(string sceneName)
     {
-        SceneManager.LoadScene(GoTo);
+        SceneManager.LoadScene(sceneName);
     }
 }
